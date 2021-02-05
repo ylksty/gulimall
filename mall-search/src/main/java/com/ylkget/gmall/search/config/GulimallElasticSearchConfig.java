5f7d1755aa8a0c1c11549ed0bb8146f24059f5f6
yang linkai <ylksty@163.com>
2021-02-05 23:13:11 +0800

    🎨 规范

---
+++ mall-search/src/main/java/com/ylkget/gmall/search/config/GulimallElasticSearchConfig.java
@@ -0,0 +1,47 @@
+package com.ylkget.gmall.search.config;
+
+import org.apache.http.HttpHost;
+import org.elasticsearch.client.RequestOptions;
+import org.elasticsearch.client.RestClient;
+import org.elasticsearch.client.RestClientBuilder;
+import org.elasticsearch.client.RestHighLevelClient;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author joe 2021-02-05 21
+ */
+
+@Configuration
+public class GulimallElasticSearchConfig {
+
+    public static final RequestOptions COMMON_OPTIONS;
+    static {
+        RequestOptions.Builder builder = RequestOptions.DEFAULT.toBuilder();
+//        builder.addHeader("Authorization", "Bearer " + TOKEN);
+//        builder.setHttpAsyncResponseConsumerFactory(
+//                new HttpAsyncResponseConsumerFactory
+//                        .HeapBufferedResponseConsumerFactory(30 * 1024 * 1024 * 1024));
+        COMMON_OPTIONS = builder.build();
+    }
+
+    @Bean
+    public RestHighLevelClient esRestClient(){
+
+        //TODO 修改为线上的地址
+        RestClientBuilder builder = null;
+        //final String hostname, final int port, final String scheme
+
+        builder = RestClient.builder(new HttpHost("192.168.31.155", 9200, "http"));
+        RestHighLevelClient client = new RestHighLevelClient(builder);
+
+//        builder = RestClient.builder(HttpHost.create(esUrl));
+
+//        RestHighLevelClient client = new RestHighLevelClient(
+//                RestClient.builder(
+//                        new HttpHost("192.168.31.155", 9200, "http")));
+
+        return client;
+    }
+}
